9f1cadb49e92c20324bccc8f7f5714ccf98e1f21
FaroukJGRNT <farouk.okanla@epitech.eu>
2025-09-14 17:22:42 +0100

    refactor: jump animations modified

--- scripts/airborne.gd
+++ scripts/airborne.gd
@@ -1,15 +1,16 @@
 extends PlayerState
 
 func enter():
-	AnimPlayer.play("jump")
+	AnimPlayer.play("fall")
 	
 func update(delta):
 	if player.is_on_wall():
 		transitioned.emit("wallsliding")
 	# Change animation to fall when descending
-	if player.velocity.y > 0 and AnimPlayer.animation == "jump":
+	if abs(player.velocity.y) < 100:
+		AnimPlayer.play("jump_peak")
+	else:
 		AnimPlayer.play("fall")
-
 	player.initiate_slide()
 	player.handle_vertical_movement(delta)
 	player.handle_horizontal_movement(player.AERIAL_SPEED)
